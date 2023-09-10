ff171f745329662ab5c6b8e5d155513e5be9d8ef
Souhaibbek <Bekkaysouhaib1996@gmail.com>
2023-09-10 18:10:54 +0100

    finish_intl_phone_ui

--- lib/screens/auth/register/register_page.dart
+++ lib/screens/auth/register/register_page.dart
@@ -1,10 +1,10 @@
 import 'package:chateo/routes/app_routes.dart';
 import 'package:chateo/screens/auth/register/register_controller.dart';
-
 import 'package:chateo/utils/buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:intl_phone_field/country_picker_dialog.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 
 class RegisterPage extends GetView<RegisterController> {
@@ -58,6 +58,27 @@ class RegisterPage extends GetView<RegisterController> {
                           disableLengthCheck: true,
                           showDropdownIcon: false,
                           cursorColor: style.primaryColor,
+                          keyboardAppearance: Theme.of(context).brightness,
+                          keyboardType: TextInputType.phone,
+                          flagsButtonMargin:
+                              const EdgeInsets.symmetric(horizontal: 4.0),
+                          pickerDialogStyle: PickerDialogStyle(
+                              searchFieldInputDecoration: InputDecoration(
+                            hintText: 'Search country',
+                            counterText: '',
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            errorBorder: InputBorder.none,
+                            disabledBorder: InputBorder.none,
+                            contentPadding: const EdgeInsets.only(
+                                left: 15, bottom: 11, top: 11, right: 15),
+                            hintStyle: style.textTheme.bodyMedium!.copyWith(
+                              color: style.hintColor,
+                            ),
+                            prefixIcon: const Icon(Icons.search),
+                            alignLabelWithHint: true,
+                          )),
                           decoration: InputDecoration(
                             counterText: '',
                             border: InputBorder.none,
@@ -76,9 +97,8 @@ class RegisterPage extends GetView<RegisterController> {
                             alignLabelWithHint: true,
                           ),
                           initialCountryCode: 'IN',
-                          onChanged: (phone) {
-                            print(phone.completeNumber);
-                          },
+                          onSubmitted: (p0) {},
+                          onChanged: (phone) {},
                         )
                       ],
                     ),
